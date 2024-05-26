c5cc9eeff819d60ef1ee3baf6562772435fc67fa
harshlangeh <harshxhtc@gmail.com>
2024-05-26 20:13:18 +0530

    calc

--- src/App.js
+++ src/App.js
@@ -3,21 +3,40 @@ import './App.css';
 
 
 function App() {
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
   
 
-  const handleButtonClick = () => {
 
-  }
+  const handleButtonClick = (value) => {
+    console.log(value)
+    if (value === '=') {
+      try {
+        setResult(eval(input));
+      } catch (error) {
+        setResult(error)
+      }
+    } else if (value === 'C') {
+      setInput("")
+      setResult("")
+    } else {
+      setInput((prev)=> prev + value)
+    }
+
+  };
+
+   console.log("result = ",result)
+
   return (
     <div className="App">
       <div className='display'>
         <input type='text' value={input} readOnly/>
+        <div>{result}</div>
       </div>
 
       <div className='buttons'>
         {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', 'C', '=', '+'].map((btn) => (
-          <button style={{height: '10vh', width: '10vh', fontSize: '3vh'}} key={btn} onClick={handleButtonClick}>{btn}</button>
+          <button style={{height: '10vh', width: '10vh', fontSize: '3vh', borderRadius: '10px'}} key={btn} onClick={()=>handleButtonClick(btn)}>{btn}</button>
         ))}
       </div>
       
